c438880dc04fd0f2736cd0aed7fa53ee535b2110
Quincyvi <44021548+Quincyvi@users.noreply.github.com>
2018-11-06 16:24:12 +0100

    Update space_freight.py

--- space_freight.py
+++ space_freight.py
@@ -15,16 +15,17 @@ class spacefreight():
         with open(filename) as csv_data:
                 reader = csv.reader(csv_data, delimiter=',')
                 next(reader)
-                val_sorted = sorted(reader, key = lambda x:float(x[1])/float(x[2]), reverse=False)
+                val_sorted = sorted(reader, key = lambda\
+                                    x:float(x[1])/float(x[2]), reverse=False)
                 for line in val_sorted:
                     parcel_id = line[0]
                     mass = float(line[1])
-                    volume = float(line [2])
+                    volume = float(line[2])
                     mass_per_vol = mass / volume
                     cargo_data = Cargo(parcel_id, mass, volume,
                                        mass_per_vol)
                     list_cargo.append(cargo_data)
-                    # print(cargo_data)
+                    print(cargo_data)
         return list_cargo
 
     def load_ships(self, filename):
@@ -44,14 +45,14 @@ class spacefreight():
                     locale.setlocale(locale.LC_ALL, 'en_US.UTF-8')
                     ship_base_costs = locale.currency(int(costs[0]) * 1000000,
                                                       grouping=True)
-                    ship_fuel = line[6]
+                    ship_fuel = float(line[6])
                     mass_per_volume = int(ship_pay_mass) / float(ship_pay_vol)
                     ship_data = Spacecraft(ship_name, ship_location,
                                            ship_pay_mass, ship_pay_vol,
                                            ship_mass, ship_base_costs,
                                            ship_fuel, mass_per_volume)
                     list_ships.append(ship_data)
-                    # print(ship_data)
+                    print(ship_data)
         return list_ships
 if __name__ == "__main__":
     space_freight = spacefreight('List1')
