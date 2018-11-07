ebb8380246a4333a4f9144419c0ffd58123f28e3
Quincyvi <44021548+Quincyvi@users.noreply.github.com>
2018-11-07 14:23:07 +0100

    update

--- space_craft.py
+++ space_craft.py
@@ -1,4 +1,4 @@
-
+from inventory import Inventory
 class Spacecraft(object):
     def __init__(self, name, nation, payload_mass, payload_volume, mass,
                 base_costs, fuel_to_weight, mass_per_volume):
@@ -11,6 +11,7 @@ class Spacecraft(object):
         self.base_costs = base_costs
         self.fuel_to_weight = fuel_to_weight
         self.mass_per_volume = mass_per_volume
+        self.inventory = Inventory()
     def __str__(self):
         return str(self.name) + ' ' + str(self.payload_mass) + ' ' + \
         str(self.payload_volume) + ' ' + str(self.mass) + ' ' +\
